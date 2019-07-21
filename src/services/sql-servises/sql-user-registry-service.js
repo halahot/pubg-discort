27952b6d025ea24d11757b36d40553f5ec81c578
Max <smouge1989@mail.ru>
2019-07-21 16:31:58 +0500

    Added PUBG Apa

---
+++ src/services/sql-servises/sql-user-registry-service.js
@@ -0,0 +1,71 @@
+import * as pool from '../../config/sql.config';
+import { CacheService } from '../';
+import { TIME_IN_SECONDS } from '../../shared/constants';
+
+const cache = new CacheService();
+
+export default class SqlUserRegisteryService {
+
+    static getTableName() {
+        return 'user_registery';    
+    } 
+
+    static getCacheKey(discordId) {
+        return `sql.userRegistery.getRegisteredUser-${discordId}`;
+    }
+
+    static async getRegisteredUser(discordId) {
+        const cacheKey = this.getCacheKey(discordId);
+        const ttl = TIME_IN_SECONDS.ONE_MINUTE;
+        const storeFunction = async () => {
+            const query = `select P.username, P.platform from ${this.getTableName} as UR left join players as P on P.id = UR.fk_players_id where discord_id = $1`;
+            const res = await pool.query(query, [discordId]);
+
+            if (res.rowCount !== 0){
+                return res.rows[0];
+            }
+            return null;
+        };
+
+        return await cache.get(cacheKey, storeFunction, ttl);
+    }
+
+    /**
+     * Adds a user  to the user_registery table if it doesn't exist already
+     * @param {string} serverId
+     */
+    static async registerUser(discordId, pubgId) {
+        const cacheKey = this.getCacheKey(discordId);
+        cache.del(cacheKey);
+
+        const query = `select * from ${this.getTableName} where discord_id = $1`;
+        const res = await pool.query(query, [discordId]);
+
+        if (res.rowCount === 0) {
+            await pool.query(`insert into ${this.getTableName}
+                (discord_id, fk_players_id)
+                values ($1, (select id from players where pubg_id=$2))`, [discordId, pubgId]
+            );
+            return true;
+        } else {
+            await pool.query(`update ${this.getTableName}
+                set
+                    fk_players_id=(select id from players where pubg_id=$2)
+                where discord_id = $1`, [discordId, pubgId]
+            );
+            return true;
+        }
+    }
+
+    /**
+     * Removes a user from the registery
+     * @param {string} discordId
+     */
+    static async unRegisterUser(discordId) {
+        const cacheKey = this.getCacheKey(discordId);
+        cache.del(cacheKey);
+
+        return pool.query(`delete from ${this.tableName} where discord_id = $1`, [discordId]);
+    }
+
+}
--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,2 +1,16 @@
+const  COMMON_MESSAGES =  {
+    REACTION_WARNING: ':warning: Bot is missing the `Text Permissions > Manage Messages` permission. Give permission for the best experience. :warning:'
+}
 
+const TIME_IN_SECONDS = {
+    ONE_MINUTE: 60, 
+    FIVE_MINUTES: ONE_MINUTE * 5,   
+    ONE_HOUR: ONE_MINUTE * 60,
+    THREE_HOUR: ONE_HOUR * 3
+}
+
+export default {
+    COMMON_MESSAGES,
+    TIME_IN_SECONDS
+}
 
